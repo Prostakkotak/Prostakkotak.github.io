5037273bd6454d50a30bee1f17f15020332ed3b4
Sergey V <47502482+Prostakkotak@users.noreply.github.com>
2019-12-17 01:03:07 +0300

    Add files via upload

--- js/nav-search-script.js
+++ js/nav-search-script.js
@@ -1,22 +1,32 @@
 (function () {
   var navSearch = document.getElementsByClassName('nav-search')[0],
-      searchCloseButton = document.getElementById('close_search_button'),
-      searchOpenButton = document.getElementById('open_search_button');
-  
-  var dropDownNav = document.getElementsByClassName('nav__menu')[0],
-      dropDownNavSearch = document.getElementsByClassName('nav-search')[0];
+    searchCloseButton = document.getElementById('close_search_button'),
+    searchOpenButton = document.getElementById('open_search_button');
 
-  document.getElementsByClassName('nav')[0].onclick = function(e) {
+  var dropDownNav = document.getElementsByClassName('nav__menu')[0],
+    dropDownNavSearch = document.getElementsByClassName('nav-search')[0];
+
+  document.getElementsByClassName('nav')[0].onclick = function (e) {
     if (e.target.id == 'open_search_button' || e.target.id == 'close_search_button') {
       navSearch.classList.toggle('nav-search_open');
       searchCloseButton.classList.toggle('current');
       searchOpenButton.classList.toggle('current');
     }
-    
+
     if (e.target.id == 'trigram' || e.target.classList.contains('trigram__line')) {
       trigram.classList.toggle('open');
       dropDownNav.classList.toggle('open');
       dropDownNavSearch.classList.toggle('open');
     }
   }
+
+  addEventListener('resize', function () {
+    if (document.body.offsetWidth <= 850) { // Если окно <= 850px то открытая строка поиска закрывается
+      if (searchCloseButton.classList.contains('current')) {
+        navSearch.classList.toggle('nav-search_open');
+        searchCloseButton.classList.toggle('current');
+        searchOpenButton.classList.toggle('current');
+      }
+    }
+  })
 }())
--- js/slider-script.js
+++ js/slider-script.js
@@ -1,6 +1,7 @@
 (function () {
   /* Массив элементов li */
-  var sliderItem = document.getElementsByClassName('slider__item');
+  var sliderItem = document.getElementsByClassName('slider__item'),
+    sliderItemWidth = sliderItem[0].offsetWidth; // Нынешняя ширина одного слайда
 
   /* Текущая и максимальная длина прокрутки слайдера */
   var maxScrollWidth = (sliderItem.length - 1) * sliderItem[0].offsetWidth + 4; // Без добавления 4 криво работает
@@ -45,4 +46,20 @@
       target = target.parentNode; // Чтобы функция не повесила всю страницу
     }
   }
+
+  addEventListener('resize', function () {
+    let currentItem = Math.floor((-(currentScrollWidth) + 4) / sliderItemWidth); // Вычисления номера слайда отображаемого на экране
+    
+    if (currentItem > 0) { // Если это не самый первый слайд, то идет перерасчет ширины прокрути для новой ширины окна браузера
+      currentScrollWidth = sliderItem[0].offsetWidth * currentItem + 4;
+      currentScrollWidth = -currentScrollWidth;
+    } else {
+      currentScrollWidth = 0;
+    }
+    
+    document.getElementsByClassName('slider__list')[0].style.marginLeft = currentScrollWidth + 'px'; // Возврат к стартовой точке
+    maxScrollWidth = (sliderItem.length - 1) * sliderItem[0].offsetWidth + 4;
+    maxScrollWidth = -maxScrollWidth; // Перерасчет максимальной ширины прокрутки
+    sliderItemWidth = sliderItem[0].offsetWidth; // Запоминаем новую текущую ширину одного слайда
+  })
 }())
